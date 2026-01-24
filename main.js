bf3d7eb6b4f091970cc6bcd54ef40fc431144fd2
habib_diu <habibmohammad905@gmail.com>
2026-01-24 22:37:21 +0600

    initial-commit

---
+++ main.js
@@ -0,0 +1,13 @@
+document.write('<h1 style="color: rgb(103, 120, 170)">Basic Javascript Lesson with h1 tag under JS</h1>');
+let name = "Habib";
+var age = 28;
+
+document.write("Name: "+name+" and Age: "+age);
+
+//Demo person deatils with const varible(let,var,const,any_var_name)
+const person_name = "Mustafizur Rahman";
+const phone = "01771708977";
+const email = "mustafiz22@gmail.com"
+const website = "mustafiziconbd.com";
+
+console.log("My Details:"+"\n"+person_name+"\n"+phone+"\n"+email+"\n"+website);
